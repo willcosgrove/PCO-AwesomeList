dc4795bc0113b3d7ec0bd711d8b41bd00a2b1ccc
Will Cosgrove <will@willcosgrove.com>
2014-06-15 17:54:07 -0500

    Implemented AwesomeList

--- lib/awesome_list.rb
+++ lib/awesome_list.rb
@@ -1,3 +1,19 @@
 class AwesomeList
 
+  def initialize(list)
+    @data = list.sort_by { |person| [-person[:awesomeness], person[:name]] }
+  end
+
+  def most_awesome_person
+    @data.first
+  end
+
+  def average_awesomeness
+    @data.map { |person| person[:awesomeness] }.reduce(:+) / @data.length
+  end
+
+  def top_awesome_people(count=10)
+    @data[0...count]
+  end
+
 end
